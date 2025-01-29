a00cb895e44356272736bed0e0dc31aefd68f3ef
Valerii <yermakvaleriy@gmail.com>
2025-01-29 14:49:41 +0200

    29.01.2025

--- HW9/goit-algo-hw-09.py
+++ HW9/goit-algo-hw-09.py
@@ -1,4 +1,4 @@
-'''
+"""
 Маємо набір монет [50, 25, 10, 5, 2, 1]. Уявіть, що ви розробляєте систему для касового апарату, яка повинна визначити
 оптимальний спосіб видачі решти покупцеві.
 
@@ -16,20 +16,24 @@
 великому та звертаючи увагу на їхню продуктивність при великих сумах. Висвітліть, як вони справляються з великими сумами
 та чому один алгоритм може бути більш ефективним за інший у певних ситуаціях. Свої висновки додайте у файл readme.md
 домашнього завдання.
-'''
+"""
+
 import time
+
+
 def find_coins_greedy(amount):
-    coins = [50, 25, 10, 5, 2, 1 ]
+    coins = [50, 25, 10, 5, 2, 1]
     result = {}
 
     for coin in coins:
         if amount >= coin:
             count = amount // coin
             result[coin] = count
-            amount -= coin*count
+            amount -= coin * count
     return result
 
-#TEST:
+
+# TEST:
 
 amount = int(input("Enter the amount: "))
 # Measure the time for drawing
@@ -39,28 +43,33 @@ print("Greedy Algorithm Result:", find_coins_greedy(amount))
 finished_time = time.time() - start_time
 print(f"Tame taken for calculating greedy algorithm: {finished_time:.16f} seconds")
 
+
 def find_min_coins(amount):
-    coins=[50,25,10,5,2,1]
-    dp=[float('inf')]*(amount+1)
-    dp[0]=0
-    coin_used=[0]*(amount+1)
+    coins = [50, 25, 10, 5, 2, 1]
+    dp = [float("inf")] * (amount + 1)
+    dp[0] = 0
+    coin_used = [0] * (amount + 1)
 
-    for i in range(1, amount+1):
+    for i in range(1, amount + 1):
         for coin in coins:
-            if i >= coin and dp[i-coin] + 1<dp[i]:
-                dp[i]=dp[i-coin]+1
-                coin_used[i]=coin
+            if i >= coin and dp[i - coin] + 1 < dp[i]:
+                dp[i] = dp[i - coin] + 1
+                coin_used[i] = coin
 
-    result={}
-    remaining=amount
-    while remaining>0:
-        coin=coin_used[remaining]
-        result[coin]=result.get(coin,0)+1
-        remaining -=coin
+    result = {}
+    remaining = amount
+    while remaining > 0:
+        coin = coin_used[remaining]
+        result[coin] = result.get(coin, 0) + 1
+        remaining -= coin
     return result
+
+
 start_time1 = time.time()
 print()
 print("Dynamic Programming Algorithm Result:", find_min_coins(amount))
 # Calculate and display time
 finished_time1 = time.time() - start_time1
-print(f"Tame taken for calculating dynamic programming algorithm: {finished_time1:.16f} seconds")
+print(
+    f"Tame taken for calculating dynamic programming algorithm: {finished_time1:.16f} seconds"
+)
